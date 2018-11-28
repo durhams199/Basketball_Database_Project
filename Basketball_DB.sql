429ca64d1cac5f277668ac3ca0e62baf356edb7a
Sebastian <durhams199@potsdam.edu>
2018-11-27 19:53:08 -0500

    Added player table

--- Basketball_DB.sql
+++ Basketball_DB.sql
@@ -1,1 +1,13 @@
 create table players
+    (playerID       int(10),
+     first_name     varchar(20) not null,
+     last_name      varchar(20) not null,
+     DOB            date(),
+     height         varchar(6),
+     weight         int(3),
+     position       varchar(14),
+     teamName       varchar(30),
+     primary key (playerID)
+     foreign key (teamName) references team(teamName)
+       );
+
